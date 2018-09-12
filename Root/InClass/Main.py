65a04b5d8a1d743dcb7e9b814241060bdeca03f6
Kwon <W0417939@campus.nscc.ca>
2018-09-12 14:54:24 -0300

    Second Updated

---
+++ Root/InClass/Main.py
@@ -0,0 +1,25 @@
+# PROG1700-JIN
+"""
+Name : Myeongjin Kwon (JIN)
+
+ID : W0417939
+
+This is a placeholder file for PROG1700 project structure. Updating the content for each task is a suggested idea.
+
+
+InClass Assignment 1.1
+
+Assignment Description: Hello World
+
+Name : Jin, Student
+ID...: W0417939
+"""
+
+__AUTHOR__ = "Jin <w0417939@nscc.ca>"
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
+
